11c2f92e02effc044a49e38b88a522d27ed2a74e
Oier Layana <layana.138303@e.unavarra.es>
2023-03-12 19:27:43 +0100

    String calculator acabado

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,5 +4,18 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+    function add(String $numbers): int {
+        if (empty($numbers)) {
+            return 0;
+        }
+
+        $numbersWithoutCommas = explode(",", $numbers);
+        $numbersWithoutNewLine = explode("\n", $numbersWithoutCommas);
+
+        $suma = 0;
+        foreach ($numbersWithoutNewLine as $number) {
+            $suma += (int) $number;
+        }
+        return $suma;
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -9,5 +9,36 @@ use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
-    // TODO: String Calculator Kata Tests
+    /**
+     * @test
+     */
+    public function returns_0_for_empty_string() {
+        $calculator = new StringCalculator();
+
+        $sum = $calculator->add("");
+
+        $this->assertEquals(0, $sum);
+    }
+
+    /**
+     * @test
+     */
+    public function returns_value_for_1_number() {
+        $calculator = new StringCalculator();
+
+        $sum = $calculator->add("1");
+
+        $this->assertEquals(1, $sum);
+    }
+
+    /**
+     * @test
+     */
+    public function returns_value_for_2_number() {
+        $calculator = new StringCalculator();
+
+        $sum = $calculator->add("1,2");
+
+        $this->assertEquals(3, $sum);
+    }
 }
